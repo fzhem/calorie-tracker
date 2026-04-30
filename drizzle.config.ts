478b66148da747252c309ac65a0f1bd28da61825
fzhem <faizanessex@gmail.com>
2026-05-01 00:05:16 +0100

    fix: update paths in drizzle configuration for correct schema and output directories

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "src/db/schema.ts",
-  out: "src/drizzle",
+  schema: "./db/schema.ts",
+  out: "./drizzle",
   dialect: "sqlite",
   driver: "expo",
   verbose: true,
